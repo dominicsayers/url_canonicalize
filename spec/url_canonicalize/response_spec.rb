3869372cdf223462184674cd87113ff4fb1514df
Dominic Sayers <dominic@sayers.cc>
2017-03-08 11:32:29 +0000

    Add response spec

---
+++ spec/url_canonicalize/response_spec.rb
@@ -0,0 +1,37 @@
+describe URLCanonicalize::Response::Redirect do
+  let(:host) { 'www.twitter.com' }
+  let(:protocol) { 'http' }
+  let(:url) { "#{protocol}://#{host}" }
+
+  describe URLCanonicalize::Response::Redirect do
+    it 'has the expected properties' do
+      response = URLCanonicalize::Response::Redirect.new(url)
+      expect(response.url).to eq(url)
+    end
+  end
+
+  describe URLCanonicalize::Response::Success do
+    it 'has the expected properties' do
+      response = URLCanonicalize::Response::Success.new(url, 'response', 'html')
+      expect(response.url).to eq(url)
+      expect(response.response).to eq('response')
+      expect(response.html).to eq('html')
+    end
+  end
+
+  describe URLCanonicalize::Response::CanonicalFound do
+    it 'has the expected properties' do
+      response = URLCanonicalize::Response::CanonicalFound.new(url, 'response')
+      expect(response.url).to eq(url)
+      expect(response.response).to eq('response')
+    end
+  end
+
+  describe URLCanonicalize::Response::Failure do
+    it 'has the expected properties' do
+      response = URLCanonicalize::Response::Failure.new('failure_class', 'message')
+      expect(response.failure_class).to eq('failure_class')
+      expect(response.message).to eq('message')
+    end
+  end
+end
